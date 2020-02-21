be06a8cafdbb5585849d316086c6647603c84fc6
Brooke Dobbins <brooke.dobbins@eagles.oc.edu>
2020-02-20 19:03:06 -0600

    Created switch case for BookSites

--- Checkmate.py
+++ Checkmate.py
@@ -2,6 +2,16 @@ from BookSite import GoogleBooks, Kobo, LibrariaCultura, Scribd, TestBookstore
 
 def get_book_site(slug):
     print("Will return one of the BookSite modules")
+    if "google" in slug:
+        return GoogleBooks
+    elif "kobo" in slug:
+        return Kobo
+    elif "librariacultura" in slug:
+        return LibrariaCultura
+    elif "scribd" in slug:
+        return Scribd
+    elif "testBookstore" in slug:
+        return TestBookstore
 
 class BookData:
     #put attributes here
