4e70cb618e3de97148e24f9ec4bc326bd6485bf8
Brooke Dobbins <brooke.dobbins@eagles.oc.edu>
2020-02-22 14:08:08 -0600

    Getting first 8 attributes for Test Bookstore

--- BookSite/TestBookstore.py
+++ BookSite/TestBookstore.py
@@ -1,10 +1,43 @@
 from BookData import BookData
+from lxml import etree
+import io
+import requests
+from PIL import Image
+
+"""Can propably be moved to common utils"""
+def get_image_from_url(element):
+    image_response = requests.get(element)
+    img = Image.open(io.BytesIO(image_response.content))
+    return img
+
+"""Can propably be moved to common utils"""
+def get_root_from_url(url):
+    content = requests.get(url).content
+    parser = etree.HTMLParser(remove_pis=True)
+    tree = etree.parse(io.BytesIO(content), parser)
+    root = tree.getroot()
+    return root
 
 """Given a direct link to a book page at a site, parse it and return the SiteBookData of the info""" 
 def get_book_data(url):
-    # type: (str) -> SiteBookData 
-    print("Get book data function from TestBookstore")
-    return BookData()
+    book_data = BookData()
+
+    root = get_root_from_url(url)
+
+    book_data.format = root.xpath(".//p[@class='details']")[0].text
+    book_data.title = root.xpath(".//p[@id='title']/strong")[0].text
+
+    """Will work if you add an image to testbook store description page """
+    #book_data.image_url = root.xpath(".//img/@src")[0]
+    #book_data.image = get_image_from_url(book_data.image_url)
+
+    book_data.isbn = root.xpath(".//span[@id='isbn']")[0].text
+    book_data.description = root.xpath("//script[@type='text/javascript']/text()")[0].split("\"")[19]
+
+    book_data.series = root.xpath(".//span[@id='series']")[0].text
+    book_data.vol_number = root.xpath(".//span[@id='volume_number']")[0].text
+
+    return book_data
 
 
 """Given a SiteBookData, search for the book at the `book_site` site
--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from Checkmate import *
 
 def testBookstore():
-    book_site = get_book_site("http://127.0.0.1:8000/library/")
+    book_site = get_book_site("TB")
     book_data = book_site.get_book_data("http://127.0.0.1:8000/library/9781524243456/")
-    book_data.format = "Digital"
     print(book_data.format)
+    print(book_data.title)
+    print(book_data.image_url)
+    print(book_data.isbn)
+    print(book_data.description)
+    print(book_data.series)
+    print(book_data.vol_number)
 
 
 if __name__ == "__main__":
