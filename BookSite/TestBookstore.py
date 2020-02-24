28aa3a2dcea5b19a9de3ab8131bb6e05a474edbd
Tmat12 <tyler.mathews12@hotmail.com>
2020-02-24 14:48:47 -0600

    Changed all queries to util function

--- BookSite/TestBookstore.py
+++ BookSite/TestBookstore.py
@@ -12,26 +12,27 @@ def get_book_data(url):
 
     root = get_root_from_url(url)
 
-    book_data.format = root.xpath(".//p[@class='details']")[0].text
-    book_data.title = root.xpath(".//p[@id='title']/strong")[0].text
+    book_data.format = queryHtml(root, ".//p[@class='details']").text
+    title = queryHtml(root, ".//p[@id='title']/strong").text
+    if ":" in title:
+        title_array = title.split(":")
+        book_data.title = title_array[0]
+        book_data.subtitle = title_array[1]
+    else:
+        book_data.title = title
 
     """Will work if you add an image to testbook store description page """
     #book_data.image_url = root.xpath(".//img/@src")[0]
     #book_data.image = get_image_from_url(book_data.image_url)
 
-    book_data.isbn = root.xpath(".//span[@id='isbn']")[0].text
-    book_data.description = root.xpath("//script[@type='text/javascript']/text()")[0].split("\"")[19]
+    book_data.isbn = str.strip(queryHtml(root, ".//span[@id='isbn']").text)
+    book_data.description = queryHtml(root, "//script[@type='text/javascript']/text()").split("\"")[19]
 
-    book_data.series = root.xpath(".//span[@id='series']")[0].text
-    book_data.vol_number = root.xpath(".//span[@id='volume_number']")[0].text
-
-    try:
-        book_data.subtitle = root.xpath(".//p[@id='title']/strong")[0].text.split(": ")[1]
-    except:
-        book_data.subtitle = "None"
+    book_data.series = queryHtml(root, ".//span[@id='series']").text
+    book_data.vol_number = queryHtml(root, ".//span[@id='volume_number']").text
     
-    book_data.authors = root.xpath(".//span[@id='author']")[0].text
-    book_data.authors = str.strip(book_data.authors)
+    authorsUnstripped = queryHtml(root, ".//span[@id='author']").text
+    book_data.authors = str.strip(authorsUnstripped)
     
     book_data.book_id = book_data.isbn
 
@@ -40,13 +41,13 @@ def get_book_data(url):
     book_data.url = convert_book_id_to_url(book_data.book_id)
     #book_data.content
 
-    book_data.ready_for_sale = root.xpath(".//i/@class")[0]
+    book_data.ready_for_sale = queryHtml(root, ".//i/@class")
     if(book_data.ready_for_sale == "fa fa-times-circle x-mark"):
         book_data.ready_for_sale = False
     else: 
         book_data.ready_for_sale = True
 
-    book_data.extra = {"price" : root.xpath(".//span[@id='price']")[0].text, "releaseDate" : root.xpath(".//span[@id='release_date']")[0].text}
+    book_data.extra = {"price" : queryHtml(root, ".//span[@id='price']").text, "releaseDate" : queryHtml(root, ".//span[@id='release_date']").text}
 
     return book_data
 
