f58619aabaa58b63fa1dc9a695904166720ea793
Brooke Dobbins <brooke.dobbins@eagles.oc.edu>
2020-02-26 14:30:37 -0600

    Added comments

--- BookSite/common/utils.py
+++ BookSite/common/utils.py
@@ -5,22 +5,49 @@ import io
 import requests
 from PIL import Image
 
-"""From Test Bookstore"""
-def get_image_from_url(element):
+def get_image_from_url(url):
+    """
+    Will get the pillow image from a url
+
+        Args:
+            url: the URL for the image
+
+        Returns:
+            A Pillow image
+    """
     image_response = requests.get(element)
     img = Image.open(io.BytesIO(image_response.content))
     return img
 
-"""From Test Bookstore"""
 def get_root_from_url(url):
+    """
+    Will get the lxml root for an html file.
+
+        Args:
+            url: the URL for the website
+
+        Returns:
+            An etree root for the page.
+    """
     content = requests.get(url).content
     parser = etree.HTMLParser(remove_pis=True)
     tree = etree.parse(io.BytesIO(content), parser)
     root = tree.getroot()
     return root
 
-"""From Kobo"""
 def queryHtml(root, expr):
+    """
+    Wrapper for node.xpath() to prevent IndexOutOfBounds Exception
+
+        Args:
+            root: the starting node for the xpath expression
+            expr: the string expression to query with
+
+        Returns:
+            - Single node for unique query
+            - Array for queries that are not unique
+            - None when the query result is empty
+    """
     try:
         result = root.xpath(expr)
         if len(result) == 1:
