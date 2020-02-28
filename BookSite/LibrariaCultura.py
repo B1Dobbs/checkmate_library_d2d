bb04c3da57ac50c827b7ec7f65e5d33b05bd370f
catherinegates97 <catherine.gates@eagles.oc.edu>
2020-02-28 14:01:13 -0600

    get book data LC added

--- BookSite/LibrariaCultura.py
+++ BookSite/LibrariaCultura.py
@@ -1,8 +1,65 @@
+from BookData import BookData
+from lxml import etree
+from BookSite.common.utils import *
+import requests, sys, webbrowser, bs4
+
 
 """Given a direct link to a book page at a site, parse it and return the SiteBookData of the info""" 
 def get_book_data(url):
-    # type: (str) -> SiteBookData 
-    print("Get book data function from Libraria")
+    book_data = BookData()
+    root = get_root_from_url(url)
+
+
+    try:
+        book_data.format = "Digital"
+        title = queryHtml(root, ".//meta[@property='og:title']/@content")
+        book_data.title = str(title)
+
+        subtitle = queryHtml(root, ".//th[contains(text(), 'Subtitulo')]/following-sibling::td")
+        if(subtitle is not None):
+            book_data.subtitle = subtitle.text
+
+        imageUrl = queryHtml(root, ".//meta[@itemprop='image']/@content")
+        book_data.image_url = str(imageUrl)
+        book_data.image = get_image_from_url(book_data.image_url)
+
+        book_data.isbn = queryHtml(root, ".//th[contains(text(), 'ISBN')]/following-sibling::td").text
+        book_data.description = str(queryHtml(root, ".//meta[@property='og:title']/following-sibling::meta[@property='og:description']/@content"))
+
+        #Series and Volume Number are not available on site
+
+        authorsTag = queryHtml(root, ".//th[contains(text(), 'Colaborado')]/following-sibling::td").text
+
+        #Parsing Author Name
+        collaborators = authorsTag.split("|")
+        matchingAuthor = [s for s in collaborators if "Autor" in s]
+
+        for a in matchingAuthor:
+            authorsNamesString = matchingAuthor[0].split(":")
+            authorsNamesArray = authorsNamesString[1].split(",")
+            # printing like a string, not a array
+            book_data.authors.append(authorsNamesArray[1].strip() + "," + authorsNamesArray[0].strip())
+
+        book_data.site_slug = "LC"
+
+        bookURL = str(queryHtml(root, ".//meta[@itemprop='url']/@content"))
+        book_data.url = bookURL
+
+        bookID = bookURL.split('.br')
+        book_data.book_id = bookID[1]
+
+        book_data.content = queryHtml(root, "/html")
+
+        book_data.ready_for_sale = True
+
+        #Price is gathered as extra data
+        book_data.extra = {"price" : str(queryHtml(root, ".//meta[@property='product:price:amount']/@content"))}
+
+    except:
+        print("ERROR: Processing book at " + url)
+        print(sys.exc_info()[0])
+
+    return book_data
 
 
 """Given a SiteBookData, search for the book at the `book_site` site
--- test.py
+++ test.py
@@ -12,7 +12,13 @@ def testKobo():
     book_data = book_site.get_book_data("https://www.kobo.com/us/en/ebook/the-alchemist-38")
     book_data.printData()
 
+def testLivrariaCultura():
+    book_site = get_book_site("LC")
+    book_data = book_site.get_book_data("https://www3.livrariacultura.com.br/what-if-its-us-2012739487/p")
+    book_data.printData()
+
 
 if __name__ == "__main__":
     testBookstore()
-    #testKobo()
+    testKobo()
+    testLivrariaCultura()
