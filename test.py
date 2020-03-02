a703765eefaa111838d249acdade4b0acb1de975
Trevor Bailey <34873770+trevor-bailey@users.noreply.github.com>
2020-03-02 01:31:50 -0600

    added google test

--- test.py
+++ test.py
@@ -12,6 +12,11 @@ def testKobo():
     book_data = book_site.get_book_data("https://www.kobo.com/us/en/ebook/the-alchemist-38")
     book_data.printData()
 
+# def testGoogle():
+#     book_site = get_book_site("GB")
+#     book_data = book_site.get_book_data("https://books.google.com/books?id=5iTebBW-w7QC")
+#     book_data.printData()
+
 
 if __name__ == "__main__":
     testBookstore()
